7829a0866479e4004b5aa0a773b9ff730868961f
chaanmi <wwkto3@gmail.com>
2024-06-27 23:48:40 +0300

    change css

--- ToDoList/ToDoList.js
+++ ToDoList/ToDoList.js
@@ -1,25 +1,41 @@
-let addToDoButton = document.getElementById("addToDo");
-let toDoContainer = document.getElementById("ToDoContainer");
-let inputField = document.getElementById("inputField");
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
 
-addToDoButton.addEventListener("click", function() {
-    var paragraph = document.createElement("p");
-    paragraph.classList.add("paragraph-styling");
-    paragraph.innerText = inputField.value;
-    toDoContainer.appendChild(paragraph);
-    inputField.value = "";
-    paragraph.addEventListener("click", function(){
-        paragraph.style.textDecoration = "line-through";
-    })
-    paragraph.addEventListener("dblclick", function(){
-        toDoContainer.removeChild(paragraph);
-    })
-})
+function addTask(){
+    if(inputBox.value === ""){
+    alert("You must write something!");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span")
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    saveData();
+}
 
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData()
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData()
+    }
+});
 
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
 
-
-
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
 
 
 
